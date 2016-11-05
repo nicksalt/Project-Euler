be70ab06907cfc11dbb62422e274c29cb820622b
Nick <nicksalt24@gmail.com>
2016-11-05 04:23:57 -0400

    Problem 47

--- Python/047/47.py
+++ Python/047/47.py
@@ -1,34 +1,56 @@
-import math, functools
-def isPrime(num):
-    if num%2==0 or num%5==0 or num==1:
-        return False
-    else:
-        for i in range(3, int(math.sqrt(num))+1):
-            if num%i==0:
-                return False
-    return True
-def findFactor(n):
-    return set(functools.reduce(list.__add__, ([i, n//i]
-                                               for i in range(1, int(math.sqrt(n)) + 1) if n % i == 0)))
-   
-  
-def find():
-    c=0
-    n=1000
+from functools import reduce
+from time import time
+def primes(goal, primes):
+    candidate = primes[len(primes)-1]+2
+    while goal > primes[len(primes)-1]:
+        limit = candidate ** 0.5
+        for p in primes:
+            if p > limit:
+                primes.append(candidate)
+                break
+            if candidate % p == 0:
+                break
+        candidate += 2
+    return primes
+
+def factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+def prime_factors(primes,total,n,time):
+    for i in primes:
+        if (n/i)%i==0:
+            i=int(n/i/i)
+        if i not in total:
+            total.append(i)
+    if len(total)==4*time:
+        return total
+    return False
+	    
+def main():
+    primelst=[2,3,5,7]
+    n=645
+    start=time()
     while True:
-        l=[]
-        facts=findFactor(n)
-        for fact in facts:
-            if isPrime(fact):
-                l.append(fact)
-        if len(l)==4:
-            c+=1
-            if len(str(n))>5:
-                print(n)
-        else:
-            c=0
-        if c==3:
-            return n-2
+        total=[]
+        primelst=primes(int(n/2)+4, primelst)
+        fac=factors(n).intersection(primelst)
+        total=prime_factors(fac,total,n,1)
+        if total!=False:
+            fac=factors(n+1).intersection(primelst)
+            total=prime_factors(fac,total,n+1,2)
+            if total!=False:
+                fac=factors(n+2).intersection(primelst)
+                total=prime_factors(fac,total,n+2,3)
+                if total!=False:
+                    fac=factors(n+3).intersection(primelst)
+                    total=prime_factors(fac,total,n+3,4)
+                    if total!=False:
+                        end=time()
+                        print(end-start)
+                        return n
+  
         n+=1
-print(find())
+                                    
+                
+        
         
